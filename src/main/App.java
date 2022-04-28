d5616aceccc5baf511a3047ed430fbdc7dfceaba
Nico Van Ommen <nicovanommen.nvo@gmail.com>
2022-04-28 02:06:42 +0200

    Java programma opgezet!

---
+++ src/main/App.java
@@ -0,0 +1,23 @@
+package main;
+
+
+import static java.lang.System.out;
+
+
+/**
+ * @author Nico van Ommen
+ * @since 04/27/2022
+ */
+public class App
+{
+    /**
+     * Methode voor het starten van het programma
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception 
+    {
+        out.println("[info]\t\tHellow World!");
+    }
+}
